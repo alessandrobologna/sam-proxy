16706f1d21fe0f2a7da3f698f31f6708ea49b8ad
Alessandro Bologna <alessandrobologna@gmail.com>
2019-07-18 12:59:46 -0400

    added ssm caching and backoff

--- src/proxy/app.py
+++ src/proxy/app.py
@@ -1,27 +1,25 @@
-import boto3
-import logging
-import json
-import io
 import base64
+import io
+import json
+import logging
 import os
 import re
-import requests
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all 
+
 import aws_lambda_logging
+import backoff
+import boto3
+import botocore
+import requests
+from aws_xray_sdk.core import patch_all, xray_recorder
+from cachetools import TTLCache, cached
 
 log = logging.getLogger()
 patch_all()
 ssm = boto3.client('ssm')
 
-def get_response_buffer(raw):
-    buffer = io.BytesIO()
-    chunk = raw.read(4096)
-    while len(chunk)>0:
-        buffer.write(chunk)
-        chunk = raw.read(4096)
-    return buffer
+ssm_cache = TTLCache(maxsize=1000, ttl=300)
 
+@xray_recorder.capture()
 def proxy_handler(event, context):
     aws_lambda_logging.setup(
         level=os.environ.get('LOGLEVEL', 'INFO'),
@@ -46,6 +44,7 @@ def proxy_handler(event, context):
     log.info(result)
     return result
 
+@xray_recorder.capture()
 def sanitize_request_headers(headers):
     # set upstream host header
     headers['Host'] = re.match('https?://([^/]+)',os.environ['UPSTREAM'])[1]
@@ -73,6 +72,11 @@ def forward_request(event):
     if body and event.get('isBase64Encoded'):
         body = base64.b64decode(body).decode('utf-8') 
         
+    return make_request(method, url, event, body)
+
+@xray_recorder.capture()
+@backoff.on_exception(backoff.expo, requests.exceptions.ConnectionError, max_time=10)
+def make_request(method, url, event, body):
     response = requests.request(
         method, 
         url,
@@ -83,23 +87,30 @@ def forward_request(event):
     )
     return response
 
+@xray_recorder.capture()
+def get_response_buffer(raw):
+    buffer = io.BytesIO()
+    chunk = raw.read(4096)
+    while len(chunk)>0:
+        buffer.write(chunk)
+        chunk = raw.read(4096)
+    return buffer
+
+@xray_recorder.capture()
 def auth_handler(event, context):
     aws_lambda_logging.setup(
         level=os.environ.get('LOGLEVEL', 'INFO'),
         aws_request_id=context.aws_request_id,
         boto_level='CRITICAL'
     )
+    authorization_user='Unknown'
+    authorization_method = 'N/A'
     try:
-        authorization_user='Unknown'
-        authorization = event['authorizationToken'] if 'TOKEN' == event['type'] else event['headers']['Authorization'] 
-        if authorization:
-            authorization_method, authorization_value = authorization.split(' ')
-            authorization = base64.b64decode(authorization_value).decode('utf-8')
-            authorization_user, authorization_password = authorization.split(':')
-            parameter = ssm.get_parameter(
-                Name=os.environ['SSM_AUTHORIZATION_PATH']+authorization_user,
-                WithDecryption=True
-            ).get('Parameter')
+        authorization_header = event['authorizationToken'] if 'TOKEN' == event['type'] else event['headers']['Authorization'] 
+        if authorization_header:
+            authorization_method, authorization_value = authorization_header.split(' ')
+            authorization_user, authorization_password = base64.b64decode(authorization_value).decode('utf-8').split(':')
+            parameter = get_ssm_param(authorization_user)
             if parameter['Value'] == authorization_password:
                 log.info({
                     'Method':authorization_method,
@@ -124,13 +135,23 @@ def auth_handler(event, context):
                 })
                 return policy
 
-            # authorization failed                
-            log.info({
-                'Method':authorization_method,
-                'User':authorization_user,
-                'Effect': 'Deny'
-            })
     except Exception as e:
         log.error(e)
 
+    # authorization failed                
+    log.info({
+        'Method':authorization_method,
+        'User':authorization_user,
+        'Effect': 'Deny'
+    })
     raise Exception('Unauthorized')
+
+@xray_recorder.capture()
+@cached(ssm_cache)
+@backoff.on_exception(backoff.expo, botocore.exceptions.ClientError, max_time=10)
+def get_ssm_param(authorization_user):
+    parameter = ssm.get_parameter(
+        Name=os.environ['SSM_AUTHORIZATION_PATH']+authorization_user,
+        WithDecryption=True
+    ).get('Parameter')
+    return parameter
